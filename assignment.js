35152090be7f888307da93f418de0b9aeca233d9
arif286 <71484413+arif286@users.noreply.github.com>
2021-01-22 22:41:12 +0600

    finished assignment

--- assignment.js
+++ assignment.js
@@ -1,30 +1,32 @@
 // https://github.com/arif286/assignment
 
+
+// kilometerToMeter function start
 function kilometerToMeter(km) {
   if (km >= 1) {
-      return km * 1000
+    return km * 1000
   } else if (km <= 0) {
-      return "Please input positive number"
+    return "Please input positive number"
   }
 }
 
-let result = kilometerToMeter()
+let result = kilometerToMeter(5)
 console.log(result);
 
 
 
-
+// budgetCalculator function start
 function budgetCalculator(x, y, z) {
   const watch = 50;
   const phone = 100;
   const laptop = 500;
   let sum = 0;
   if ((x >= 0) && (y >= 0) && (z >= 0)) {
-      sum = x * watch
-      sum = sum + (y * phone);
-      sum = sum + (z * laptop);
+    sum = x * watch
+    sum = sum + (y * phone);
+    sum = sum + (z * laptop);
   } else if ((x < 0) || (y < 0) || (z < 0)) {
-      return "Oh! no, please input positive number."
+    return "Oh! no, please input positive number."
   }
   return sum
 }
@@ -35,22 +37,22 @@ console.log(result);
 
 
 
-
+// hotelCost function start
 function hotelCost(day) {
   let totalCost = 0;
   if (day <= 10) {
-      totalCost = day * 100;
+    totalCost = day * 100;
   } else if (day <= 20) {
-      let firstTenDay = 10 * 100;
-      let remainingDay = day - 10;
-      let secondTenDay = remainingDay * 80;
-      totalCost = firstTenDay + secondTenDay;
+    let firstTenDay = 10 * 100;
+    let remainingDay = day - 10;
+    let secondTenDay = remainingDay * 80;
+    totalCost = firstTenDay + secondTenDay;
   } else {
-      let firstTenDay = 10 * 100;
-      let secondTenDay = 10 * 80;
-      let remainingDay = day - 20;
-      let thirdTenDay = remainingDay * 50;
-      totalCost = firstTenDay + secondTenDay + thirdTenDay;
+    let firstTenDay = 10 * 100;
+    let secondTenDay = 10 * 80;
+    let remainingDay = day - 20;
+    let thirdTenDay = remainingDay * 50;
+    totalCost = firstTenDay + secondTenDay + thirdTenDay;
   }
   return totalCost
 }
@@ -58,12 +60,12 @@ let result = hotelCost(2);
 console.log(result);
 
 
-
+//megaFriend function start
 function megaFriend(array) {
   let longestWord = "";
 
-  array.forEach(function(word) {
-    if(word.length > longestWord.length) {
+  array.forEach(function (word) {
+    if (word.length > longestWord.length) {
       longestWord = word;
     }
   });
@@ -71,5 +73,5 @@ function megaFriend(array) {
   return longestWord;
 }
 
-let word = megaFriend(["The","quick","brown", "fox", "jumped", "over", "the", "lazy", "dog"]);
+let word = megaFriend(["Habu", "Hablu", "Kabila", "Sokina", "Montu", "Jorina", ]);
 console.log(word);
