ef50ff7fb084f829bfb8a314beebc9d8ca9b8ad8
arif286 <71484413+arif286@users.noreply.github.com>
2021-01-22 15:48:39 +0600

    completed my js assignment

---
+++ assignment.js
@@ -0,0 +1,73 @@
+// https://github.com/arif286/assignment
+
+function kilometerToMeter(km) {
+    if (km >= 1) {
+        return km * 1000
+    } else if (km <= 0) {
+        return "Please input positive number"
+    }
+}
+
+let result = kilometerToMeter()
+console.log(result);
+
+
+
+
+function budgetCalculator(x, y, z) {
+    const watch = 50;
+    const phone = 100;
+    const laptop = 500;
+    let sum = 0;
+    if ((x >= 0) && (y >= 0) && (z >= 0)) {
+        sum = x * watch
+        sum = sum + (y * phone);
+        sum = sum + (z * laptop);
+    } else if ((x < 0) || (y < 0) || (z < 0)) {
+        return "Oh! no, please input positive number."
+    }
+    return sum
+}
+
+let result = budgetCalculator(1, 5, 6)
+console.log(result);
+
+
+
+
+
+function hotelCost(day) {
+    let totalCost = 0;
+    if (day <= 10) {
+        totalCost = day * 100;
+    } else if (day <= 20) {
+        let firstTenDay = 10 * 100;
+        let remainingDay = day - 10;
+        let secondTenDay = remainingDay * 80;
+        totalCost = firstTenDay + secondTenDay;
+    } else {
+        let firstTenDay = 10 * 100;
+        let secondTenDay = 10 * 80;
+        let remainingDay = day - 20;
+        let thirdTenDay = remainingDay * 50;
+        totalCost = firstTenDay + secondTenDay + thirdTenDay;
+    }
+    return totalCost
+}
+let result = hotelCost(2);
+console.log(result);
+
+
+function megaFriend(str) {
+    let longestWord = "";
+    for (let i = 0; i < str.length; i++) {
+        let currentItem = str[i]
+        if (currentItem.length > longestWord.length) {
+            result = str[i];
+        }
+    }
+    return result;
+}
+const element = ['kablu', 'Habu', 'kabila', 'kacumacu']
+let words = megaFriend(element)
+console.log(words);
